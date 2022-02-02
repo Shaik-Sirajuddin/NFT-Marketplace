68e9c5a348b938989cd3fa93919399726dbf79ac
buttowski <08irfanmd@gmail.com>
2022-02-02 15:07:36 +0530

    added progress bar,buy button,changed image block

--- src/mainComponents/ItemDetail.js
+++ src/mainComponents/ItemDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from '../components/components/footer';
 import { createGlobalStyle } from 'styled-components';
+import ProgressButton from 'react-progress-button'
 
 import { useLocation } from "@reach/router";
 
@@ -11,66 +12,76 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 
-  
 
-const Colection= function(props){
 
-// const location = useLocation()
-const data = props.location.state.from;
-console.log(data);
+const Colection = function (props) {
 
-const [openMenu, setOpenMenu] = React.useState(true);
-const [openMenu1, setOpenMenu1] = React.useState(false);
-const handleBtnClick = () => {
-  setOpenMenu(!openMenu);
-  setOpenMenu1(false);
-  document.getElementById("Mainbtn").classList.add("active");
-  document.getElementById("Mainbtn1").classList.remove("active");
-};
-const handleBtnClick1 = () => {
-  setOpenMenu1(!openMenu1);
-  setOpenMenu(false);
-  document.getElementById("Mainbtn1").classList.add("active");
-  document.getElementById("Mainbtn").classList.remove("active");
-};
-return (
-<div>
-<GlobalStyles/>
+  // const location = useLocation()
+  const data = props.location.state.from;
+  console.log(data);
 
-  <section className='container'>
-    <div className='row mt-md-5 pt-md-4'>
+  const [openMenu, setOpenMenu] = React.useState(true);
+  const [openMenu1, setOpenMenu1] = React.useState(false);
+  const handleBtnClick = () => {
+    setOpenMenu(!openMenu);
+    setOpenMenu1(false);
+    document.getElementById("Mainbtn").classList.add("active");
+    document.getElementById("Mainbtn1").classList.remove("active");
+  };
+  const handleBtnClick1 = () => {
+    setOpenMenu1(!openMenu1);
+    setOpenMenu(false);
+    document.getElementById("Mainbtn1").classList.add("active");
+    document.getElementById("Mainbtn").classList.remove("active");
+  };
 
-    <div className="col-md-6 text-center">
-                            <img src={data.image} className="img-fluid img-rounded mb-sm-30" alt=""/>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="item_info">
-                               
-                                <h2>{data.title}</h2>
-                                <div className="item_info_counts">
-                                    <div className="item_info_type"><i className="fa fa-image"></i>Art</div>
-                                    
-                                </div>
-                                <p>{data.description}</p>
+  const handleClick3 = ()=>{
+    return new Promise(function(resolve, reject) {
+      setTimeout(resolve, 3000)
+    })
+  };
 
-                               
+  return (
+    <div>
+      <GlobalStyles />
 
-                                <div className="spacer-40"></div>
+      <section className='container'>
+        <div className='row mt-md-5 pt-md-4'>
 
-                                <div className="de_tab">
-    
-                            
-                                
-                            </div>
-                                
-                            </div>
-                        </div>
+          
 
+          <div className="col-md-6 text-center">
+            <img src={data.image} style={{ "maxBlockSize": 350 }} className="img-fluid img-rounded mb-sm-30" alt="" />
+          </div>
+          <div className="col-md-6">
+            <div className="item_info">
+
+              <h2>{data.title}</h2>
+              <div className="item_info_counts">
+                <div className="item_info_type"><i className="fa fa-image"></i>Art</div>
+
+              </div>
+              <p>{data.description}</p>
+              <input type="button" value="Buy Now" className="btn-main" />
+
+
+
+              <div className="spacer-40"></div>
+
+              <div className="de_tab">
+
+
+
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <Footer />
     </div>
-  </section>
-
-  <Footer />
-</div>
-);
+  );
 }
 export default Colection;
